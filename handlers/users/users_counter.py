94a6726c7eb8f34c1d7cb17e288d83f67f794426
TolibjonDev <104207059+TolibjonDev@users.noreply.github.com>
2023-11-23 12:15:48 +0500

    Create users_counter.py

---
+++ handlers/users/users_counter.py
@@ -0,0 +1,18 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart, IDFilter
+from aiogram.dispatcher.filters import Command
+import json
+from data.config import ADMINS
+from loader import dp,bot
+
+@dp.message_handler(Command("users", prefixes="/:"), IDFilter(chat_id=ADMINS[0]))
+async def show_bot_users(msg: types.Message):
+        await msg.answer("salom Admin")
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+            users_count = len(data['users'])
+            gr_count = len(data['groups'])
+            ch_count = len(data['channels'])
+        with open("data.json", 'rb') as file:
+            await msg.answer_document(document=file, caption=f"<b>All users id:</b>\n---------------\nUsers count: {users_count}\nGroups count: {gr_count}\nChannels count: {ch_count}")
+          
